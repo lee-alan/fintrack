e6018d1505f3f76ccc6bb7168aa34843a9a2f503
Ram Gurram <abhi6ama@gmail.com>
2020-02-29 14:50:57 -0500

    adding react components

---
+++ fintrack/frontend/fintrack/src/pages/expenses.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+class ExpensesPage extends Component {
+  state = {};
+  render() {
+    return <h1>ExpensesPage</h1>;
+  }
+}
+
+export default ExpensesPage;
