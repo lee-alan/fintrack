9a2d9aa58a11746d314e2da041c0a6084fc920db
Ram Gurram <abhi6ama@gmail.com>
2020-04-10 20:54:59 -0400

    adding links to about page

--- fintrack/frontend/src/pages/about.jsx
+++ fintrack/frontend/src/pages/about.jsx
@@ -15,22 +15,22 @@ class AboutPage extends Component {
         <h4>Made Possible by:</h4>
         <div>
           <ul>
-            <li>React</li>
+            <li><a href="https://reactjs.org/">React</a></li>
             <ul>
-              <li>Material UI</li>
-              <li>Plotly</li>
+              <li><a href="https://material-ui.com/">Material UI</a></li>
+              <li><a href="https://plotly.com/javascript/react/">Plotly</a></li>
             </ul>
             <li>Backend</li>
             <ul>
-              <li>Express</li>
-              <li>Node Mailer</li>
-              <li>Mongo DB</li>
+              <li><a href="https://expressjs.com/">Express</a></li>
+              <li><a href="https://nodemailer.com/about/">Node Mailer</a></li>
+              <li><a href="https://www.mongodb.com/">MongoDB</a></li>
             </ul>
             <li>API</li>
             <ul>
-              <li>AlphaVantage - Stock Charts</li>
-              <li>WorldTradingData - Stock Information</li>
-              <li>NewAPI - Related news section</li>
+              <li><a href="https://www.alphavantage.co/">AlphaVantage</a> - Stock Charts</li>
+              <li><a href="https://www.worldtradingdata.com/">WorldTradingData</a> - Stock Information</li>
+              <li><a href="https://newsapi.org/">NewsAPI</a> - Related news section</li>
             </ul>
           </ul>
               
